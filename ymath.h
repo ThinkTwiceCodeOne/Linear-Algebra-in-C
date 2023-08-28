ade4f2c75ec29f58cc347453680057f7978c0167
ThinkTwiceCodeOne <112020385+ThinkTwiceCodeOne@users.noreply.github.com>
2023-08-28 00:25:46 -0400

    Add files via upload

--- ymath.h
+++ ymath.h
@@ -1,6 +1,82 @@
 #include<stdio.h>
 #include<stdbool.h>
 #include<stdlib.h>
+double determinant(int** matrix,int r,int c);
+double** inverse(int** matrix,int r,int c)
+{
+double matrix1[r][c];
+if(r==c)
+{
+double **matrix2=(double **)malloc(r*sizeof(double *));
+for(int i=0;i<r;i++)
+{
+matrix2[i]=(double *)malloc(c*sizeof(double));
+}
+if(determinant(matrix,r,c)==0.0) 
+{
+printf("For this matrix inverse is not possible\n");
+return NULL;
+}
+for(int i=0;i<r;i++)
+{
+for(int j=0;j<c;j++)
+{
+matrix1[i][j]=(double)matrix[i][j];
+if(i==j) matrix2[i][j]=1.0;
+else matrix2[i][j]=0.0;
+}
+}
+for(int i=0;i<r-1;i++)
+{
+for(int j=i+1;j<c;j++)
+{
+double ratio=0.0;
+if(matrix1[i][i]!=0)
+{
+ratio=(double)matrix1[j][i]/matrix1[i][i];
+}
+else ratio=(double)matrix[j][i];
+for(int k=0;k<r;k++)
+{
+matrix1[j][k]=matrix1[j][k]-(ratio*matrix1[i][k]);
+matrix2[j][k]=matrix2[j][k]-(ratio*matrix2[i][k]);
+}
+}
+}
+for(int i=0;i<r-1;i++)
+{
+for(int j=i+1;j<r;j++)
+{
+double ratio;
+if(matrix1[j][j]!=0)
+{
+ratio=(double)matrix1[i][j]/matrix1[j][j];
+}
+else ratio=(double)matrix1[i][j];
+for(int k=0;k<r;k++)
+{
+matrix1[i][k]=matrix1[i][k]-(ratio*matrix1[j][k]);
+matrix2[i][k]=matrix2[i][k]-(ratio*matrix2[j][k]);
+}
+}
+}
+for(int i=0;i<r;i++)
+{
+double temp=matrix1[i][i];
+for(int j=0;j<r;j++)
+{
+matrix2[i][j]=matrix2[i][j]/temp;
+}
+}
+return matrix2;
+}
+else
+{
+printf("Number of rows and columns should be equal");
+return NULL;
+}
+return NULL;
+}
 double determinant(int** matrix,int r,int c)
 {
 double result=1;
@@ -19,7 +95,7 @@ for(int i=0;i<r-1;i++)
 for(int j=i+1;j<c;j++)
 {
 double ratio=(double)matrix1[j][i]/matrix1[i][i];
-printf("ratio is %f\n",ratio);
+//printf("ratio is %f\n",ratio);
 for(int k=0;k<r;k++)
 {
 matrix1[j][k]=matrix1[j][k]-(ratio*matrix1[i][k]);
@@ -27,19 +103,19 @@ matrix1[j][k]=matrix1[j][k]-(ratio*matrix1[i][k]);
 //printf("%d %d\n",j,i);
 }
 }
-for(int i=0;i<r;i++)
+/*for(int i=0;i<r;i++)
 {
 for(int j=0;j<c;j++)
 {
 printf("%f ",matrix1[i][j]);
 }
 printf("\n");
-}
+}*/
 for(int i=0;i<r;i++)
 {
 if(matrix1[i][i]==0) return 0;
 result=result*matrix1[i][i];
-printf("result is %f\n",result);
+//printf("result is %f\n",result);
 }
 }
 else
