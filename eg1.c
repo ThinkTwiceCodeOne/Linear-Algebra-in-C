a16413722a745599f8fed36379a5e48d90f0ab97
ThinkTwiceCodeOne <112020385+ThinkTwiceCodeOne@users.noreply.github.com>
2023-08-29 11:04:38 -0400

    Add files via upload

--- eg1.c
+++ eg1.c
@@ -1,14 +1,10 @@
 #include<stdio.h>
-#include "ymath.h"
-void printMatrix(int **matrix3,int r1,int c1)
+#include "ylinAlg.h"
+void printMatrix(double *matrix3,int r1)
 {
 for(int i=0;i<r1;i++)
 {
-for(int j=0;j<c1;j++)
-{
-printf("%d ",matrix3[i][j]);
-}
-printf("\n");
+printf("%lf\n",matrix3[i]);
 }
 }
 int main()
@@ -24,21 +20,25 @@ for(int i=0;i<3;i++)
 {
 matrix2[i]=(int *)malloc(3*sizeof(int));
 }
-int **matrix3=(int **)malloc(3*sizeof(int *));
+double **matrix3=(double **)malloc(3*sizeof(double *));
 for(int i=0;i<3;i++)
 {
-matrix3[i]=(int *)malloc(3*sizeof(int));
+matrix3[i]=(double *)malloc(3*sizeof(double));
 }
 for(int i=0;i<3;i++)
 {
 for(int j=0;j<3;j++)
 {
-matrix1[i][j]=count;
-matrix2[i][j]=count;
-count++;
+scanf("%d",&matrix1[i][j]);
 }
 }
-matrix3=addMatrix(matrix1,matrix2,3,3,3,3);
-printMatrix(matrix3,3,3);
+double* matrix5=(double *)malloc(3*sizeof(double));
+int* matrix4=(int *)malloc(3*sizeof(int));
+for(int i=0;i<3;i++)
+{
+scanf("%d",&matrix4[i]);
+}
+matrix5=rootsOfEquations(matrix1,matrix4,3,3);
+printMatrix(matrix5,3);
 return 0;
 }
